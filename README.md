f5583434de1d5214b3e5123bc9aab5f732d9997b
Karan Sakhuja <karan.sakhuja@outlook.com>
2023-12-26 09:42:16 -0500

    Improvements

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # gh-actions
 
+You must enable workflow permissions on the organization level and then on the repository level.
+
+> <img alt="image" width="800" src="https://github.com/pricelastic/gh-actions/assets/926720/faea32df-cf14-4435-9e27-2686836115f0">
+
+---
+
 ## 1. Create Promotion PR
 
-Automatically creates pull requests for promoting changes from staging to production branch.
+Automatically creates pull requests for promoting changes from staging to the production branch.
 
 - Uses GitHub CLI to create a pull request
 - Add a workflow file `.github/workflows/promotion-pr.yml` in your repository
@@ -15,11 +21,17 @@ on:
     types: [closed]
 jobs:
   Create-Prod-Promotion-PR:
-    uses: pricelastic/gh-actions/create-promotion-pr@main
-    with:
-      stagingBranch: main
-      prodBranch: prod
-      repoName: ${{ github.event.repository.full_name }}
-    env:
-      GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+    runs-on: ubuntu-latest
+    permissions:
+      pull-requests: write
+      contents: write
+      repository-projects: read
+    steps:
+      - uses: pricelastic/gh-actions/create-promotion-pr@main
+        with:
+          stagingBranch: main
+          prodBranch: prod
+          repoName: ${{ github.event.repository.full_name }}
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
 ```
--- create-promotion-pr/script.sh
+++ create-promotion-pr/script.sh
@@ -1,32 +1,31 @@
-createProdPromotionPR() {
-  local REPO_NAME=$1
-  local STAGING_BRANCH=$2
-  local PROD_BRANCH=$3
-  local PR_BODY="## Prod Promotion PRs\n\n"
+REPO_NAME=$1
+STAGING_BRANCH=$2
+PROD_BRANCH=$3
+PR_BODY="## Prod Promotion PRs
+"
 
-  # Check for existing promotion PR
-  local PROMOTION_PR=$(gh pr list --state=open \
-    --repo=$REPO_NAME --base=$PROD_BRANCH \
-    --json number --jq '. | sort_by(.updatedAt) | reverse | .[0].number')
+# Check for existing promotion PR
+PROMOTION_PR=$(gh pr list --state=open \
+  --repo=$REPO_NAME --base=$PROD_BRANCH \
+  --json number --jq '. | sort_by(.updatedAt) | reverse | .[0].number')
 
-  # Create a promotion PR if none exists
-  if [ -z "$PROMOTION_PR" ]; then
-    PROMOTION_PR=$(gh pr create --repo=$REPO_NAME \
-      --base=$PROD_BRANCH --head=$STAGING_BRANCH \
-      --title="🚀 $STAGING_BRANCH ⮕ $PROD_BRANCH" \
-      --body="$PR_BODY")
+# Create a promotion PR if none exists
+if [ -z "$PROMOTION_PR" ]; then
+  PROMOTION_PR=$(gh pr create --repo=$REPO_NAME \
+    --base=$PROD_BRANCH --head=$STAGING_BRANCH \
+    --title="🚀 $STAGING_BRANCH ⮕ $PROD_BRANCH" \
+    --body="$PR_BODY")
 
-    echo "PROMOTION_PR created: $PROMOTION_PR"
-  else
-    echo "PROMOTION_PR exists: $PROMOTION_PR"
-  fi
+  echo "PROMOTION_PR created: $PROMOTION_PR"
+else
+  echo "PROMOTION_PR exists: $PROMOTION_PR"
+fi
 
-  # Pull all the staging PRs
-  local STAGING_PRS=$(gh pr list --state=open --repo=$REPO_NAME \
-    --base=$STAGING_BRANCH \
-    --json number,title,updatedAt,author \
-    --jq 'sort_by(.updatedAt) | reverse | .[] | "- #\(.number): \(.title) @\(.author.login)"')
+# Pull all the staging PRs
+STAGING_PRS=$(gh pr list --state=open --repo=$REPO_NAME \
+  --base=$STAGING_BRANCH \
+  --json number,title,updatedAt,author \
+  --jq 'sort_by(.updatedAt) | reverse | .[] | "- #\(.number): \(.title) @\(.author.login)"')
 
-  # Update the promotion with PR body + staging PRs
-  gh pr edit $PROMOTION_PR --repo=$REPO_NAME --body="$PR_BODY$STAGING_PRS"
-}
+# Update the promotion with PR body + staging PRs
+gh pr edit $PROMOTION_PR --repo=$REPO_NAME --body="$PR_BODY$STAGING_PRS"

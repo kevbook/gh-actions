2c751adb058a0c03858d28b7090d8c7fbb90e0c3
Karan Sakhuja <karan.sakhuja@outlook.com>
2023-12-26 13:17:18 -0500

    Update script.sh

--- create-promotion-pr/script.sh
+++ create-promotion-pr/script.sh
@@ -1,7 +1,11 @@
 # Script arguments
-REPO_NAME=$1
+REPO_NAME_FULL=$1
 STAGING_BRANCH=$2
 PROD_BRANCH=$3
+
+# Splitting REPO_NAME into owner and repo
+REPO_OWNER="${REPO_NAME_FULL%%/*}"
+REPO_NAME="${REPO_NAME_FULL##*/}"
 
 # Templates
 PR_TITLE="🚀 $STAGING_BRANCH ⮕ $PROD_BRANCH"
@@ -10,12 +14,12 @@ PR_BODY="## Prod Promotion PRs
 
 # Check for existing promotion PR
 PROMOTION_PR=$(gh pr list --state=open \
-  --repo=$REPO_NAME --base=$PROD_BRANCH \
+  --repo=$REPO_NAME_FULL --base=$PROD_BRANCH \
   --json number --jq '. | sort_by(.updatedAt) | reverse | .[0].number')
 
 # Create a promotion PR if none exists
 if [ -z "$PROMOTION_PR" ]; then
-  PROMOTION_PR=$(gh pr create --repo=$REPO_NAME \
+  PROMOTION_PR=$(gh pr create --repo=$REPO_NAME_FULL \
     --base=$PROD_BRANCH --head=$STAGING_BRANCH \
     --title="$PR_TITLE" --body="$PR_BODY")
 
@@ -24,13 +28,33 @@ else
   echo "PROMOTION_PR exists: $PROMOTION_PR"
 fi
 
-# Pull all the staging PRs
-STAGING_PRS=$(gh pr list --state=open --repo=$REPO_NAME \
-  --base=$STAGING_BRANCH \
-  --json number,title,updatedAt,author \
-  --jq 'sort_by(.updatedAt) | reverse | .[] | "- #\(.number): \(.title) @\(.author.login)"')
-
-echo "STAGING_PRS: $STAGING_PRS"
+# Use graph API to pull all the commits for the PR
+# Format: #pr: message @author 
+STAGING_PRS=$(gh api graphql -f query="{
+  repository(owner: \"${REPO_OWNER}\", name: \"${REPO_NAME}\") {
+    pullRequest(number: 20) {
+      title
+      commits(first: 100) {
+        nodes {
+          commit {
+            author {
+              user {
+                login
+              }
+            }
+            message
+            associatedPullRequests(first: 1) {
+              nodes {
+                number
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}" --jq '.data.repository.pullRequest.commits.nodes | map(select(.commit.associatedPullRequests.nodes | length > 0)) | map("- #\(.commit.associatedPullRequests.nodes[0].number): \(.commit.message) @\(.commit.author.user.login)") | .[]')
 
 # Update the promotion PR
-gh pr edit $PROMOTION_PR --repo=$REPO_NAME --title="$PR_TITLE" --body="$PR_BODY$STAGING_PRS"
+echo "STAGING_PRS: $STAGING_PRS"
+gh pr edit $PROMOTION_PR --repo=$REPO_NAME_FULL --title="$PR_TITLE" --body="$PR_BODY$STAGING_PRS"

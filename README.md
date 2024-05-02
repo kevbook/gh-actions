f0099fb8baccb507b9a835935a2638081f1538ac
Karan Sakhuja <karan.sakhuja@outlook.com>
2024-05-01 21:46:22 -0400

    Minor improvements (#1)

--- README.md
+++ README.md
@@ -1,28 +1,52 @@
 # gh-actions
 
-You must enable workflow permissions on the organization level and then on the repository level.
+You must enable workflow permissions on the organization level
 
 > <img alt="image" width="800" src="https://github.com/pricelastic/gh-actions/assets/926720/faea32df-cf14-4435-9e27-2686836115f0">
 
----
+## Development and Local Testing
+
+1. [VSCode Plugin](https://marketplace.visualstudio.com/items?itemName=GitHub.vscode-github-actions)
+2. [Act](https://github.com/nektos/act) to test Github Actions locally
+   - Docker issue: https://github.com/nektos/act/issues/2239#issuecomment-1979819940
+
+```shell
+# Test the action locally using act
+$ act --secret GITHUB_TOKEN \
+    --workflows=./tests/create-promotion-pr.yml \
+    --eventpath=tests/pr-merged.json
+```
 
 ## 1. Create Promotion PR
 
 Automatically creates pull requests for promoting changes from staging to the production branch.
 
+- Uses [GitHub ToolKit](https://github.com/actions/toolkit)
 - Uses GitHub CLI to create a pull request
-- Add a workflow file `.github/workflows/promotion-pr.yml` in your repository
+- Add the below workflow file `.github/workflows/promotion-pr.yml` in your repository
+
+```shell
+# Start the local server
+$ pnpm run dev
+
+# Create a local production build
+$ pnpm run build
+```
 
 ```yaml
+# .github/workflows/promotion-pr.yml
 name: Automatic PRs for main ⮕ prod
 on:
+  # On push to main branch or PR closed event
+  push:
+    branches: [main]
   pull_request:
     branches: [main]
     types: [closed]
 jobs:
   Create-Prod-Promotion-PR:
     runs-on: ubuntu-latest
-    # Action uses GitHub CLI requiring the below permissions
+    # Action uses GitHub API requiring the below permissions
     permissions:
       pull-requests: write
       contents: write
@@ -32,7 +56,16 @@ jobs:
         with:
           stagingBranch: main
           prodBranch: prod
-          repoName: ${{ github.event.repository.full_name }}
         env:
           GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
 ```
+
+---
+
+## References
+
+1. https://github.com/actions/toolkit
+2. https://github.com/sdras/awesome-actions
+3. https://github.com/actions/javascript-action
+4. https://github.com/docker/build-push-action
+5. https://github.com/softprops/action-gh-release
---
+++ create-promotion-pr/run.js
@@ -0,0 +1,100 @@
+import github from '@actions/github';
+import core from '@actions/core';
+
+/**
+ * Run function for the action
+ */
+async function run() {
+  // Only run on push to staging branch or PR merged
+  if (!(github.context.eventName === 'push' || github.context.payload.pull_request?.merged)) {
+    core.error('Action did not meet criteria to run (push to staging branch or PR merged)');
+    return;
+  }
+
+  // Get inputs
+  const stagingBranch = core.getInput('stagingBranch');
+  const prodBranch = core.getInput('prodBranch');
+  core.info(`Staging branch: ${stagingBranch}`);
+  core.info(`Production branch: ${prodBranch}`);
+
+  // Repo context
+  const { owner, repo } = github.context.repo;
+  core.info(`Owner: ${owner}`);
+  core.info(`Repo: ${repo}`);
+
+  // Rest client instance
+  const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+
+  // Check for existing promotion PR (prod branch)
+  let prId;
+  const { data: prs } = await octokit.rest.pulls.list({
+    owner,
+    repo,
+    state: 'open',
+    base: prodBranch,
+  });
+
+  // Create prod PR if none exists
+  if (prs?.length) {
+    prId = prs[0].number;
+    core.info(`Prod promotion PR exists: #${prId}`);
+  } else {
+    // Pull the oldest commit on the staging branch
+    const { data: commits } = await octokit.rest.repos.listCommits({
+      owner,
+      repo,
+      sha: stagingBranch,
+      per_page: 100,
+    });
+
+    // Create prod branch if it doesn't exist (from oldest commit on staging)
+    try {
+      // Fail safely if branch already exists
+      const createdBranch = await octokit.rest.git.createRef({
+        owner,
+        repo,
+        ref: `refs/heads/${prodBranch}`,
+        sha: commits.pop().sha, // Last item
+      });
+      core.info(`"${prodBranch}" branch created: #${createdBranch.number}`);
+    } catch (e) {}
+
+    // Create prod PR
+    const { data: createdPR } = await octokit.rest.pulls.create({
+      owner,
+      repo,
+      title: `🚀 ${stagingBranch} ⮕ ${prodBranch}`,
+      head: stagingBranch,
+      base: prodBranch,
+    });
+    prId = createdPR.number;
+    core.info(`Prod promotion PR created: #${prId}`);
+  }
+
+  // Get commits for the PR
+  const { data: commits } = await octokit.rest.pulls.listCommits({
+    owner,
+    repo,
+    pull_number: prId,
+  });
+  // core.debug(`Commits: ${JSON.stringify(commits)}`);
+
+  // Build PR body, format: "- {parent.id}: {message} @{author}"
+  const commentsArr = commits.map(function (i) {
+    // Convert multiline message to single line
+    const message = i.commit.message.replace(/\t+/g, '').replace(/(\r?\n)+/g, '. ');
+    return `- ${i.parents[0]?.sha}: ${message} @${i.author.login}`;
+  });
+  // Add title H2
+  commentsArr.unshift('## Prod Promotion PRs\n');
+
+  await octokit.rest.pulls.update({
+    owner,
+    repo,
+    pull_number: prId,
+    body: commentsArr.join('\n'),
+  });
+  core.info(`Prod promotion PR body updated: #${prId}`);
+}
+
+run();

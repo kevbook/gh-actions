3b0c48d71c307ddd5c249f04f14da7bcfc94cace
Karan Sakhuja <karan.sakhuja@outlook.com>
2024-05-09 17:25:27 -0400

    Fixes

--- ci-helper/run.js
+++ ci-helper/run.js
@@ -1,6 +1,7 @@
 import { EOL } from 'node:os';
 import core from '@actions/core';
 import exec from '@actions/exec';
+import github from '@actions/github';
 
 /**
  * Run function for the action
@@ -15,11 +16,20 @@ async function run() {
   const secretsFile = core.getInput('secretsFile');
   core.info(`secretsFile: ${secretsFile}`);
 
+  // Get commit message via REST client
+  const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+  const { data } = await octokit.rest.repos.getCommit({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    ref: github.context.sha,
+  });
+
   // Set skip deploy if commit message contains [skip deploy]
   // https://github.com/orgs/vercel/discussions/60#discussioncomment-114386
   // https://samanpavel.medium.com/github-actions-output-parameters-f7de80922712
-  const { stdout: gitMessage } = await exec.getExecOutput('git log -1 --pretty=oneline');
-  core.setOutput('skip_deploy', /\[skip deploy\]/i.test(gitMessage) ? '1' : '0');
+  const commitMessage = data.commit.message;
+  core.info(`Commit message: ${commitMessage}`);
+  core.setOutput('skip_deploy', /\[skip deploy\]/i.test(commitMessage) ? '1' : '0');
 
   // Execute op-secrets command to get secrets
   const { stdout, stderr } = await exec.getExecOutput(`op-secrets --config ${secretsFile} env`, {
